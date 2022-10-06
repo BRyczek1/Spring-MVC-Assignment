407b63fbd8bf515b6a1c8dc79829b64ac99d156f
BRyczek1 <89106037+BRyczek1@users.noreply.github.com>
2022-10-06 14:53:42 -0500

    Add files via upload

---
+++ Spring MVC Assignment/src/main/java/edu/wctc/croquet/GlossaryTerm.java
@@ -0,0 +1,21 @@
+package edu.wctc.croquet;
+
+/**
+ * Complete this class to hold data from croquetGlossary.json
+ */
+public class GlossaryTerm {
+    String term;
+    String definition;
+    String regulation;
+
+    public String getDefinition() {
+        return definition;
+    }
+    public String getRegulation() {
+        return regulation;
+
+    }
+    public String getTerm() {
+        return term;
+    }
+}
